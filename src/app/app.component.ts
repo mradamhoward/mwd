790d0abf4901bc398d2cd9ff253f54b684d0536e
Adam Howard <adam.howard@mycit.ie>
2020-04-05 00:00:41 +0100

    8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mwd';
+
+constructor(private renderer: Renderer2){
+
 }
+
+  
+}
+
+ { 
+  
+} 
